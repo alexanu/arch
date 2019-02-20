6cbf3a42c817b128aa029bb5d2ebb2754e046d66
Kevin Sheppard <kevin.k.sheppard@gmail.com>
2019-02-20 11:05:18 +0000

    Some additional work on simulating critical values for EG

--- arch/unitroot/__init__.py
+++ arch/unitroot/__init__.py
@@ -2,5 +2,7 @@ from __future__ import absolute_import, division
 
 from arch.unitroot.unitroot import (ADF, DFGLS, KPSS, PhillipsPerron,
                                     VarianceRatio, ZivotAndrews)
+from arch.unitroot.cointegration import DynamicOLS
 
-__all__ = ['ADF', 'KPSS', 'DFGLS', 'VarianceRatio', 'PhillipsPerron', 'ZivotAndrews']
+__all__ = ['ADF', 'KPSS', 'DFGLS', 'VarianceRatio', 'PhillipsPerron', 'ZivotAndrews',
+           'DynamicOLS']
